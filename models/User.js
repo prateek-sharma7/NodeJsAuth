0e6be593fc8f8ec494ffeb20c0a116a31e672ef8
Prateek <sharmaprateek499@gmail.com>
2024-07-07 14:44:22 +0530

    hashing the password string using bcrypt module

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const { isEmail } = require("validator");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -18,15 +19,16 @@ const userSchema = new mongoose.Schema({
 
 // This pre and post HOOKS in MONGOOSE will help to achieve password hashing
 
-// A function used to alert that a user has been created.
-userSchema.post("save", (doc, next) => {
-  console.log("new user has been created and saved", doc);
-  next();
-});
+// // A function used to alert that a user has been created.
+// userSchema.post("save", (doc, next) => {
+//   console.log("new user has been created and saved", doc);
+//   next();
+// });
 
 // A function used to alert that a user is about to create.
-userSchema.pre("save", function (next) {
-  console.log("New user is about to be created", this);
+userSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password, salt);
   next();
 });
 
